fa6a0593c2e5d8df2565fffa3b264df978e218bf
Fabianny Nunes Andrade <fabyhoshigakisama@gmail.com>
2020-03-24 02:07:21 -0300

    primeiro commit

---
+++ src/br/fai/clinica/Main.java
@@ -0,0 +1,15 @@
+package br.fai.clinica;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main app = new Main();
+		app.start();
+
+	}
+
+	private void start() {
+		System.out.println(": V");
+	}
+
+}
